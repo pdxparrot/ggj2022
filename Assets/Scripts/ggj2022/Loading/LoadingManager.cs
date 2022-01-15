1083139ea45726cc8b3439c9be61955e179a9b00
Shane Lillie <ignignokterr@gmail.com>
2022-01-15 14:49:37 -0800

    Project start

---
+++ Assets/Scripts/ggj2022/Loading/LoadingManager.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+using pdxpartyparrot.Game.Loading;
+
+namespace pdxpartyparrot.ggj2022.Loading
+{
+    public sealed class LoadingManager : LoadingManager<LoadingManager>
+    {
+        //[Space(10)]
+
+        #region Manager Prefabs
+
+        //[Header("Project Manager Prefabs")]
+
+        // TODO: project manager prefabs go here
+
+        #endregion
+
+        protected override void CreateManagers()
+        {
+            base.CreateManagers();
+
+            // TODO: create project prefabs
+        }
+    }
+}
---
+++ Assets/Scripts/ggj2022/State/SceneTester.cs
@@ -0,0 +1,12 @@
+namespace pdxpartyparrot.ggj2022.State
+{
+    public class SceneTester : Game.State.SceneTester
+    {
+        public override void InitViewers()
+        {
+            // TODO: allocate and initialize viewers
+
+            // TODO: initialize game UI
+        }
+    }
+}
