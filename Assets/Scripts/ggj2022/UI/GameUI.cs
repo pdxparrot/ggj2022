229353fcfd2cb50322fabd2b62df983c3adfae85
Shane Lillie <ignignokterr@gmail.com>
2022-01-21 12:53:58 -0800

    Pause menu
    Player HUD skeleton

--- Assets/Scripts/ggj2022/UI/GameUI.cs
+++ Assets/Scripts/ggj2022/UI/GameUI.cs
@@ -4,5 +4,9 @@ namespace pdxpartyparrot.ggj2022.UI
 {
     public sealed class GameUI : Game.UI.GameUI
     {
+        [SerializeField]
+        private PlayerHUD _playerHUD;
+
+        public PlayerHUD PlayerHUD => _playerHUD;
     }
 }
---
+++ Assets/Scripts/ggj2022/UI/PlayerHUD.cs
@@ -0,0 +1,8 @@
+using pdxpartyparrot.Game.UI;
+
+namespace pdxpartyparrot.ggj2022.UI
+{
+    public sealed class PlayerHUD : HUD
+    {
+    }
+}
